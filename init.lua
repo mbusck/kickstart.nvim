4aae63f3cf1eae7ff43a096d7835c79d17ed0f09
Mattias Busck <mattias@busck.net>
2024-07-18 11:42:55 +0200

    Add plugins

--- init.lua
+++ init.lua
@@ -225,6 +225,10 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  'tpope/vim-fugitive',
+  'vimwiki/vimwiki',
+  'mfukar/robotframework-vim',
+  'christoomey/vim-tmux-navigator',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
